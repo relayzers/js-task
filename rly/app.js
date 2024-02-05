bb7a631d198d79558c7f157abe4cc7ed12600c92
relayzers <151905250+relayzers@users.noreply.github.com>
2024-02-05 18:46:26 +0400

    Add files via upload

---
+++ rly/app.js
@@ -0,0 +1,15 @@
+// let dogumtarixi = prompt("dogum tarixini yaz")
+// let indikiil = 2024
+// indikiil-=dogumtarixi
+// console.log(indikiil);
+
+// let km = prompt("km daxil edin")
+// let mil = 0.6214
+// let mildeyeri = km * mil
+// console.log(mildeyeri);
+
+// let uzunlug = prompt("daxil et")
+// let en = prompt("en daxil et")
+
+// uzunlug *= en
+// console.log(uzunlug);
